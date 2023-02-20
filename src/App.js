43fb87b3e096448334bacea3428df0efbd7a67ed
udemba godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-02-20 14:47:45 +0100

    created te header components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import ForgotPassword from "./Pages/ForgotPassword";
 import Offers from "./Pages/Offers";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
