6861f6c9bf08d1d2ac7894c0728b2a25ecb65316
Udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-03-27 12:53:19 +0100

    create the end message on category page

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -113,7 +113,12 @@ export default function Category() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>
+          There is no current{" "}
+          {params.categoryName === "rent"
+            ? "places for rent"
+            : " places for sale"}
+        </p>
       )}
     </div>
   );
